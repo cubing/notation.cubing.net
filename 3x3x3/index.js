dd6c59273b19be48b42d6e5d8f3fb3537418fd15
Lucas Garron <code@garron.net>
2023-02-16 02:37:57 -0800

    Fix up animations and flash on click.

--- 3x3x3/index.js
+++ 3x3x3/index.js
@@ -9,23 +9,40 @@ for (const moveDisplay of document.querySelectorAll(".move-display")) {
 	twistyPlayer.cameraLatitude = 20;
 	twistyPlayer.cameraLongitude = 25;
 
+	function flash() {
+		twistyPlayer.animate(
+			[{ opacity: 1 }, { opacity: 0.2, offset: 0.3 }, { opacity: 1 }],
+			{
+				duration: 250,
+			},
+		);
+	}
+
 	const firstMoveDuration = (async () =>
 		(await twistyPlayer.experimentalModel.indexer.get()).moveDuration(0))();
 	twistyPlayer.experimentalModel.timestampRequest.set(firstMoveDuration);
 
-	const listener = () => {
+	moveDisplay.addEventListener("mouseenter", () => {
 		twistyPlayer.hintFacelets = "floating";
 		twistyPlayer.timestamp = 0;
 		twistyPlayer.experimentalModel.playingInfo.set({
 			playing: true,
 			loop: true,
 		});
-	};
-	moveDisplay.addEventListener("mouseenter", listener);
+	});
+	moveDisplay.addEventListener("click", () => {
+		twistyPlayer.hintFacelets = "floating";
+		twistyPlayer.timestamp = 0;
+		twistyPlayer.pause();
+		twistyPlayer.play();
+		flash();
+	});
 	moveDisplay.addEventListener("mouseleave", () => {
 		twistyPlayer.hintFacelets = "none";
-		twistyPlayer.pause();
+		twistyPlayer.experimentalModel.playingInfo.set({
+			playing: false,
+			loop: false,
+		});
 		twistyPlayer.experimentalModel.timestampRequest.set(firstMoveDuration);
 	});
-	moveDisplay.addEventListener("click", listener);
 }
